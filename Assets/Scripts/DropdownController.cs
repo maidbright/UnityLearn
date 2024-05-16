00c8be421b4b028a235dd52f32d105e0a89433d2
maidbright <145804413+maidbright@users.noreply.github.com>
2024-05-16 21:42:56 +0300

    corrected

--- Assets/Scripts/DropdownController.cs
+++ Assets/Scripts/DropdownController.cs
@@ -1,18 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class DropdownController : MonoBehaviour, IPointerClickHandler
 {
-    public Dropdown menu;
+    public TMP_Dropdown menu;
     private GameObject tmpGO;
     void Start()
     {
-        menu = GetComponent<Dropdown>();
+        menu = GetComponent<TMP_Dropdown>();
         tmpGO = menu.GetComponent<InfoController>().CreateTMPObject();//
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-        tmpGO = menu.GetComponent<InfoController>().SetTMPObject(tmpGO, menu.itemText.ToString());//
+        tmpGO = menu.GetComponent<InfoController>().SetTMPObject(tmpGO, menu.name);//
     }
 }
