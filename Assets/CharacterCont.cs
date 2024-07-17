2c497fbf773c2e2198b26cf3b6f1b31914495009
ECHTP-49-13\User <mail>
2024-07-17 21:42:12 +0300

    123

--- Assets/CharacterCont.cs
+++ Assets/CharacterCont.cs
@@ -7,14 +7,22 @@ public class CharacterCont : MonoBehaviour
     public float movementSpeed = 10.0f;
     public float rotationSpeed = 0.2f;
 
+    Animator animator;
     CharacterController characterController;
     Camera charCamera;
     float rotationAngle = 0.0f;
+    bool isMoving = false;
 
 public CharacterController CharacterController
     { get { return characterController = characterController ?? GetComponent<CharacterController>(); } }
 public Camera CharCamera
     { get { return charCamera = charCamera ?? FindObjectOfType<Camera>(); } }
+
+    public Animator Animator
+    {
+        get { return animator = animator ?? GetComponent<Animator>(); 
+    }
+}
 
     // Update is called once per frame
     void Update()
@@ -28,8 +36,22 @@ public Camera CharCamera
         CharacterController.Move(rotatedMovement * movementSpeed * Time.deltaTime);
 
         if(rotatedMovement.sqrMagnitude > 0.0f)
-        {
+        { 
+            if(!isMoving)
+            {
+                Animator.Play("Run");
+            }
+            isMoving = true;
+        
             rotationAngle = Mathf.Atan2(rotatedMovement.x, rotatedMovement.y) * Mathf.Rad2Deg;
+        }
+        else
+        {
+            if(isMoving) 
+            {
+                Animator.Play("Idle");
+            }
+            isMoving = false;
         }
         Quaternion currRotation = characterController.transform.rotation;
         Quaternion targRotation = Quaternion.Euler(0.0f, rotationAngle, 0.0f);
