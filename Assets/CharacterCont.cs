a876b7737b559197e73ca26b988ac963cac521f7
ECHTP-49-13\User <mail>
2024-07-17 20:58:19 +0300

    1st

---
+++ Assets/CharacterCont.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCont : MonoBehaviour
+{
+    public float movementSpeed = 10.0f;
+    public float rotationSpeed = 0.2f;
+
+    CharacterController characterController;
+    Camera charCamera;
+    float rotationAngle = 0.0f;
+
+public CharacterController CharacterController
+    { get { return characterController = characterController ?? GetComponent<CharacterController>(); } }
+public Camera CharCamera
+    { get { return charCamera = charCamera ?? FindObjectOfType<Camera>(); } }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float horiz = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horiz,0.0f,vert);
+        Vector3 rotatedMovement = Quaternion.Euler(0.0f, CharCamera.transform.rotation.eulerAngles.y,0.0f)*movement.normalized; //roation in movement
+
+        CharacterController.Move(rotatedMovement * movementSpeed * Time.deltaTime);
+
+        if(rotatedMovement.sqrMagnitude > 0.0f)
+        {
+            rotationAngle = Mathf.Atan2(rotatedMovement.x, rotatedMovement.y) * Mathf.Rad2Deg;
+        }
+        Quaternion currRotation = characterController.transform.rotation;
+        Quaternion targRotation = Quaternion.Euler(0.0f, rotationAngle, 0.0f);
+        characterController.transform.rotation = Quaternion.Lerp(currRotation, targRotation, rotationSpeed);
+    }
+}
